25a1a10c69038e5926846cf2ed4f12f1c1bb6118
AlexC <acortojan@gmail.com>
2025-01-07 11:34:32 +0200

    create generateNewCanvas function (move file)

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("MORR")
+    generateNewCanvas(16)
+})
+
+function generateNewCanvas(size){
+    let newCanvas = document.createElement("div");
+    newCanvas.classList.add("canvas")
+    for(let i = 0; i < size; i++){
+
+        let row = document.createElement("div");
+        row.classList.add("row");
+
+        for(let j = 0; j < size; j++){
+
+            let col = document.createElement("div");
+
+            col.classList.add("col");
+            col.style.backgroundColor = rgb(randomColor, randomColor, randomColor);
+            col.style.opacity = 0;
+
+            col.addEventListener("mouseover", changeColor)
+        }
+        newCanvas.appendChild(row);
+    }
+
+    let canvas = document.querySelector(".canvas")
+    canvas.remove()
+
+    document.querySelector(".container").appendChild(newCanvas)
+}
